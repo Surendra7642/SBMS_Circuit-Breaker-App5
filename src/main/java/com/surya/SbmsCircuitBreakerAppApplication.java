5632a031b071c23efaedda4837510aadf7f27f27
Surendra7642 <komarasurendra76@gmail.com>
2023-09-25 19:40:57 +0530

    circuitBreaker5

---
+++ src/main/java/com/surya/SbmsCircuitBreakerAppApplication.java
@@ -0,0 +1,21 @@
+package com.surya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableHystrix
+public class SbmsCircuitBreakerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsCircuitBreakerAppApplication.class, args);
+	}
+
+}
